6777624677c28531b15a701746366a70c8c1a3d0
Danang Rifai <danang050402@gmail.com>
2024-11-10 20:15:20 +0700

    update env

--- src/main.ts
+++ src/main.ts
@@ -29,6 +29,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('auth')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
